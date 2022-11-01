00755a57437b9be0c6fee9c49e7dc7cc505ee767
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-11-01 15:34:42 +0530

    a,b=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l3=[] l4=[]
    for i in l1:
        if i not in l3:
            l3.append(i)
            
    for i in l2:
        if i not in l4:
            l4.append(i)
    c=0      
    for i in l3:
        if i not in l4:
            print(i,end=" ")
    for i in l4:
        if i not in l3:
            print(i,end=" ")

---
+++ Common_elements_II.py
@@ -0,0 +1,20 @@
+a,b=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=[]
+l4=[]
+
+for i in l1:
+    if i not in l3:
+        l3.append(i)
+        
+for i in l2:
+    if i not in l4:
+        l4.append(i)
+c=0      
+for i in l3:
+    if i not in l4:
+        print(i,end=" ")
+for i in l4:
+    if i not in l3:
+        print(i,end=" ")
