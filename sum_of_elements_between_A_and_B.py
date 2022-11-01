df1af3e1ae46d762faeda50fedae0be6dbc06ab5
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-11-01 14:25:50 +0530

    n=int(input()) l=list(map(int,input().split())) a,b=map(int,input().split()) lst=[] for i in l:     if i>=a and i<=b:         lst.append(i) if len(lst)==0:     print(-1) else:     print(sum(lst))

---
+++ sum_of_elements_between_A_and_B.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+a,b=map(int,input().split())
+lst=[]
+for i in l:
+    if i>=a and i<=b:
+        lst.append(i)
+if len(lst)==0:
+    print(-1)
+else:
+    print(sum(lst))
