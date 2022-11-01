545f43f35c1dfad55a2014935af5d3f15eb5caba
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-11-01 15:29:26 +0530

    a,b=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l3=[] l4=[]
    for i in l1:
        if i not in l3:
            l3.append(i)
            
    for i in l2:
        if i not in l4:
            l4.append(i)
    c=0      
    for i in l3:
        if i not in l4:
            c+=1
    for i in l4:
        if i not in l3:
            c+=1
    print(c)

---
+++ Count_elements_II.py
@@ -0,0 +1,21 @@
+a,b=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=[]
+l4=[]
+
+for i in l1:
+    if i not in l3:
+        l3.append(i)
+        
+for i in l2:
+    if i not in l4:
+        l4.append(i)
+c=0      
+for i in l3:
+    if i not in l4:
+        c+=1
+for i in l4:
+    if i not in l3:
+        c+=1
+print(c)
